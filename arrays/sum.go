a24fc442dd6ad14b07fc5d3a7f7c70b3f3d429fd
Stella Leal <stellalleal@gmail.com>
2022-12-30 20:31:30 -0300

    test passes with range

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,11 @@
 package main
 
-//Sum takes an array [5]int and returns the sum of the numbers inside it
-func Sum(numbers [5]int) int{
-	return 0
+// Sum takes an array [5]int and returns the sum of the numbers inside it
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum+= number
+	}
+
+	return sum
 }
