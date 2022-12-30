933934b8a345e984a6699ff2545abc9920ce7a79
Stella Leal <stellalleal@gmail.com>
2022-12-30 20:14:49 -0300

    add result of practice exercises (i think)

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,11 +1,12 @@
 package iteration
 
-// Repeat takes a character and returns a string with it repeated 5 times
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated += character
-	}
+import "strings"
 
-	return repeated
+// Repeat takes a character and returns a string with it repeated 5 times
+func Repeat(character string, count int) string {
+	// var repeated string
+	// for i := 0; i < count; i++ {
+	// 	repeated += character
+	// }
+	return strings.Repeat(character, count)
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,28 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+// go test -bench=. -benchtime 100x
+func BenchmarkRepeat(b *testing.B) {
+	for i:= 0; i < b.N; i++ {
+		Repeat("a", b.N)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("ba", 2)
+	fmt.Println(repeated)
+	// Output: baba
 }
