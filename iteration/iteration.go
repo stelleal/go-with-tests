b4400a5a9995f6a312de360f7b99bb2ff6fb97a4
Stella Leal <stellalleal@gmail.com>
2022-12-30 19:43:14 -0300

    now the iteration test passes

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,6 +1,11 @@
 package iteration
 
-//Repeat takes a character and returns a string with it repeated 5 times
-func Repeat(character string) string{
-	return ""
+// Repeat takes a character and returns a string with it repeated 5 times
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+
+	return repeated
 }
