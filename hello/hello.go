2bcf17a67277782eb8f487de4df488fba6226508
Stella Leal <stellalleal@gmail.com>
2022-12-30 17:33:37 -0300

    Refactor the Hello function to work with empty string

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello sends a greeting string
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
