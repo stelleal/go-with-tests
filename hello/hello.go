6bdbf98f3724e3d55d3af8f65b10e70addf75725
Stella Leal <stellalleal@gmail.com>
2022-12-30 18:13:17 -0300

    Add Portuguese variation to Hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+const portuguese = "Portuguese"
 const englishHelloPrefix = "Hello, "
+const portugueseHelloPrefix = "Olá, "
 
 // Hello sends a greeting string
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == portuguese {
+		return portugueseHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,16 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Gopher")
+		got := Hello("Gopher", "")
 		want := "Hello, Gopher"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Portuguese", func(t *testing.T) {
+		got := Hello("Ana", "Portuguese")
+		want := "Olá, Ana"
+		
 		assertCorrectMessage(t, got, want)
 	})
 
