f623c00f878512d235a595c7a413a12a86b07166
Stella Leal <stellalleal@gmail.com>
2023-01-02 15:11:59 -0300

    add Area and its test passing

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,5 +2,11 @@ package main
 
 // Perimeter calculates the perimeter of a given rectangle
 func Perimeter(width, height float64) float64{
-	return 0
+
+	return (width + height) * 2
+}
+
+// Area calculates the area of a given rectangle
+func Area(width, height float64) float64{
+	return width * height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -11,3 +11,12 @@ func TestPerimeter(t *testing.T) {
 		// The f is for our float64 and the .2 means print 2 decimal places.
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
