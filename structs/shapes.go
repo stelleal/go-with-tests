1c64d60b5f085c8b92518b90fab0d46ce3592e67
Stella Leal <stellalleal@gmail.com>
2023-01-02 18:17:37 -0300

    apply the Rectangle struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,12 +1,18 @@
 package main
 
-// Perimeter calculates the perimeter of a given rectangle
-func Perimeter(width, height float64) float64{
+// Rectangle has the dimensions of a rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
 
-	return (width + height) * 2
+// Perimeter calculates the perimeter of a given rectangle
+func Perimeter(rectangle Rectangle) float64 {
+
+	return (rectangle.Width + rectangle.Height) * 2
 }
 
 // Area calculates the area of a given rectangle
-func Area(width, height float64) float64{
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
