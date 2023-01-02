85ec27e75d12c0b4d62360f5a30ee18d4405b11a
Stella Leal <stellalleal@gmail.com>
2023-01-02 18:41:37 -0300

    change Area to be a method and add interface to it

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,18 +1,35 @@
 package main
 
+import "math"
+
+// Shape is implemented by anything that can tell us its Area
+type Shape interface {
+	Area() float64
+}
+
 // Rectangle has the dimensions of a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+// Area returns the area of the rectangle.
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle represents a circle
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of the circle.
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
 // Perimeter calculates the perimeter of a given rectangle
 func Perimeter(rectangle Rectangle) float64 {
 
 	return (rectangle.Width + rectangle.Height) * 2
-}
-
-// Area calculates the area of a given rectangle
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
