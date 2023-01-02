b554cbde4a7028af887afef1a343df751c0dab37
Stella Leal <stellalleal@gmail.com>
2023-01-02 15:05:33 -0300

    add Perimeter test with minimum to compile

---
+++ structs/shapes.go
@@ -0,0 +1,6 @@
+package main
+
+// Perimeter calculates the perimeter of a given rectangle
+func Perimeter(width, height float64) float64{
+	return 0
+}
---
+++ structs/shapes_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+		// The f is for our float64 and the .2 means print 2 decimal places.
+	}
+}
