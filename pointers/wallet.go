880e2b38bf4f773c80d3cd0a8636deb15985e943
Stella Leal <stellalleal@gmail.com>
2023-01-03 11:12:24 -0300

    add Wallet test with minimum to compile

---
+++ pointers/wallet.go
@@ -0,0 +1,15 @@
+package main
+
+// Wallet stores the number of Bitcoin someone owns
+type Wallet struct {
+}
+
+// Deposit will add some Bitcoin to a wallet
+func (w Wallet) Deposit(amount int) {
+
+}
+
+// Balance returns the number of Bitcoin a wallet has
+func (w Wallet) Balance() int {
+	return 0
+}
